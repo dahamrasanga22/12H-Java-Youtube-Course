5ab712bc10e7c725ee143cf438400019b301ea7e
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-02-26 11:48:06 +0530

    Java Test

---
+++ TEST/test1.java
@@ -0,0 +1,13 @@
+public class test1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 30;
+
+        int result = x + y;
+        System.out.println("x + y = " + result);
+
+        result = x - y;
+        System.out.println("x - y = " + result);
+
+    }
+}
