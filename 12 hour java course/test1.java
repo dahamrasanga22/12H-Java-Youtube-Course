01a03621e1b07826501645f9893804d50fef17a8
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-01 23:25:42 +0530

    12H-java-course

---
+++ 12 hour java course/test1.java
@@ -0,0 +1,11 @@
+public class test1 {
+
+    public static void main(String[] args) {
+
+        System.out.print("I like pizza!");
+        System.out.print("I like pizza!");
+
+        
+    }
+    
+}
