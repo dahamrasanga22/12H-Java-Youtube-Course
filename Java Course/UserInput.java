ec477423031417eeb44a983926c2e44d0d67333e
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-02 01:30:05 +0530

    12H-Java-Youtube-Course

---
+++ Java Course/UserInput.java
@@ -0,0 +1,11 @@
+
+public class UserInput {
+    public static void main(String[] args) {
+       
+        
+        
+
+        
+    }
+    
+}
---
+++ Java Course/boolean1.java
@@ -0,0 +1,22 @@
+public class boolean1 {
+   
+    public static void main(String[] args) {
+    
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        // STU
+        if (isStudent) {
+            System.out.println("Yes u r Student");
+        } else {
+            System.out.println("Yes u r not Student");
+        }
+
+        // SALE
+        if (forSale) {
+            System.out.println("Yes=");
+        } else {
+            System.out.println("Noooooooooo");
+        }
+    }
+}
