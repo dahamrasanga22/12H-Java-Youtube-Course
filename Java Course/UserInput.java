83fc5c0eac92a85d02bc77fd1419852907c5e3dc
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-02 22:20:44 +0530

    12H-Java-Youtube-Course

--- Java Course/UserInput.java
+++ Java Course/UserInput.java
@@ -1,12 +1,23 @@
 
-public class UserInput {
-  
-    public static void main(String[] args) {
-       
-        
-        
+import java.util.Scanner;
 
-        
+public class UserInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hellooo " + name);
+
+
+        System.out.print("Enter Age : ");
+        int age = scanner.nextInt();
+        System.out.println("My age is " + age);
+
+        scanner.close();
+
     }
-    
+
 }
