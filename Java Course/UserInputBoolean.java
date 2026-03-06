98338b5ec364b88f2ae96cb1594155a01633c07c
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-06 13:09:15 +0530

    12H-Java-Youtube-Course

--- Java Course/UserInputBoolean.java
+++ Java Course/UserInputBoolean.java
@@ -10,7 +10,15 @@ public class UserInputBoolean {
         System.out.print("Are you a Student? ( true / false ): ");
         boolean isStudent = scanner.nextBoolean();
 
+        
         System.out.println("Student : " + isStudent);
+
+
+        if (isStudent) {
+            System.out.println("You are enrolled as Student");
+        } else {
+            System.out.println("You are NOT enrolled ");
+        }
 
         scanner.close();
     }
