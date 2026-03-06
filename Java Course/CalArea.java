54e6aee21242664571838a33ac72700519ef2914
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-06 13:10:51 +0530

    12H-Java-Youtube-Course

---
+++ Java Course/CalArea.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CalArea {
+    public static void main(String[] args) {
+
+        // cal area of rectangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width : ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height : ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("The area is : " + area + "cm²"); // alt + 0178 = x²
+
+        scanner.close();
+    }
+
+}
---
+++ Java Course/IntString.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class IntString {
+    public static void main(String[] args) {
+
+        // common issues
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age : ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter your fav Color : ");
+        String color = scanner.nextLine();
+
+        System.out.println("your are " + age + " years old");
+        System.out.println("You like the color " + color);
+
+        scanner.close();
+
+    }
+
+}
