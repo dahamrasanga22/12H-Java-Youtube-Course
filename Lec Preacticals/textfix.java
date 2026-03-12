97a69588e5b4d5e83113bd891c61cd5981948850
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-03-12 23:53:15 +0530

    JAVA-test

---
+++ Lec Preacticals/textfix.java
@@ -0,0 +1,18 @@
+
+class Car {
+    int speed;
+    String color;
+    double regularPrice;
+
+    double getSalePrice() {
+        return regularPrice;
+    }
+}
+
+public class MyOwnAutoshop {
+    public static void main(String[] args) {
+
+        Audi myAudi = new Audi( 122, "Red", 20000,  25);
+        System.out.println("Audi Sale Price: " + myAudi.getSalePrice());
+    }
+}
