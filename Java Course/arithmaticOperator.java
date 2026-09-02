ccaac54c475cbd8a07ca07b1080e44f7c7d45352
Daham Rasanga <dahamrasanga22@gmail.com>
2026-09-02 22:12:54 +0530

    Add files via upload

---
+++ Java Course/arithmaticOperator.java
@@ -0,0 +1,20 @@
+public class arithmaticOperator {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // z=x+y;
+        // z=x-y;
+          z=x*y;
+        // z=x/y;
+        // z=x%y;
+
+        
+        System.out.println(z);
+
+    }
+
+}
