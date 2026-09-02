bd339a9818af272c0d1c32d49fb4af1655a24674
Daham Rasanga <dahamrasanga22@gmail.com>
2026-09-02 22:18:31 +0530

    Add files via upload

---
+++ Java Course/arithmaticAssignmentOperator.java
@@ -0,0 +1,25 @@
+public class arithmaticAssignmentOperator {
+    public static void main(String[] args) {
+      
+
+     int x = 10;
+     int y = 5;
+
+
+        x+=y; // x=x+y;
+        //x-=y; // x=x-y;
+        //x*=y; // x=x*y;
+        //x/=y; // x=x/y;
+        //x%=y; // x=x%y;
+
+
+        System.out.println(x);
+
+
+
+
+
+
+
+    }
+}
